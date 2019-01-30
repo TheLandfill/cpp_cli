5f4bd95a8d5414fc0e516149d0b93af7d059222c
Joseph Mellor <joseph.mellor1999@gmail.com>
2019-01-30 05:34:42 -0600

    Initial Commit

---
+++ cpp_command_line_parser/includes/parser.h
@@ -0,0 +1,67 @@
+#ifndef CPP_CMD_LINE_PARSER_H
+#define CPP_CMD_LINE_PARSER_H
+#include <vector>
+
+class Command_Line_Var_Interface {
+protected:
+	bool takes_args_var;
+	void * base_variable;
+	std::vector<const char *> aliases;
+public:
+	Command_Line_Var_Interface(void * b_v, std::vector<const char *> a, bool ta);
+	const std::vector<const char *>& get_aliases() const {
+		return aliases;
+	}
+	bool takes_args();
+	virtual void set_base_variable(const char * b_v) = 0;
+};
+
+template<typename T>
+class Command_Line_Var : public Command_Line_Var_Interface {
+public:
+	Command_Line_Var(T * b_v, std::vector<const char *> a, bool ta) : Command_Line_Var_Interface(b_v, a, ta) {}
+	virtual void set_base_variable(const char * b_v) {
+		*(T *)base_variable = b_v;
+	}
+};
+
+template<typename T>
+struct Command_Line_Var_Holder {
+private:
+	static std::vector<Command_Line_Var<T>> command_line_var_holder;
+public:
+	Command_Line_Var_Interface * add_to_command_line_var_holder(T * var_addr, std::vector<const char *> aliases, bool ta) {
+		command_line_var_holder.emplace_back(var_addr, aliases, ta);
+		return &(command_line_var_holder.back());
+	}
+};
+
+std::vector<const char *> hash(int argc, char ** argv);
+
+template<>
+void Command_Line_Var<int>::set_base_variable(const char * b_v);
+
+template<>
+void Command_Line_Var<unsigned int>::set_base_variable(const char * b_v);
+
+template<>
+void Command_Line_Var<long>::set_base_variable(const char * b_v);
+
+template<>
+void Command_Line_Var<unsigned long>::set_base_variable(const char * b_v);
+
+template<>
+void Command_Line_Var<long long>::set_base_variable(const char * b_v);
+
+template<>
+void Command_Line_Var<unsigned long long>::set_base_variable(const char * b_v);
+
+template<>
+void Command_Line_Var<float>::set_base_variable(const char * b_v);
+
+template<>
+void Command_Line_Var<double>::set_base_variable(const char * b_v);
+
+template<>
+void Command_Line_Var<long double>::set_base_variable(const char * b_v);
+#endif
