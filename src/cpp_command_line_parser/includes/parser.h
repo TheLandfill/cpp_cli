7d7103ec7b7345bb1f3a3efb3de1aa91e59d0a18
Joseph Mellor <joseph.mellor1999@gmail.com>
2019-02-04 03:21:22 -0600

    Removed unimplemented and unnecessary struct.

--- src/cpp_command_line_parser/includes/parser.h
+++ src/cpp_command_line_parser/includes/parser.h
@@ -35,17 +35,6 @@ public:
 	virtual void set_base_variable(const char * b_v);
 };
 
-template<typename T>
-struct Command_Line_Var_Holder {
-private:
-	static std::vector<Command_Line_Var<T>> command_line_var_holder;
-public:
-	Command_Line_Var_Interface * add_to_command_line_var_holder(T * var_addr, std::vector<const char *> aliases, bool ta) {
-		command_line_var_holder.emplace_back(var_addr, aliases, ta);
-		return &(command_line_var_holder.back());
-	}
-};
-
 std::vector<const char *> hash_cmd_line_into_variables(int argc, char ** argv, size_t num_unique_flags = 1000);
 
 template<>
